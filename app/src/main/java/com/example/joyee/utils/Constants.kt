998935bbab269088005cfa7be4c3e0d6b4298e6c
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2022-09-02 10:45:50 +0200

    add icons assets

---
+++ app/src/main/java/com/example/joyee/utils/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.joyee.utils
+
+import com.example.joyee.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_home,
+    R.drawable.ic_bee,
+    R.drawable.ic_bug,
+    R.drawable.ic_car,
+    R.drawable.ic_cloud,
+    R.drawable.ic_face,
+    R.drawable.ic_flower,
+    R.drawable.ic_flowers,
+    R.drawable.ic_icecream,
+    R.drawable.ic_puzzle,
+    R.drawable.ic_sun,
+    R.drawable.ic_toys
+)
+
