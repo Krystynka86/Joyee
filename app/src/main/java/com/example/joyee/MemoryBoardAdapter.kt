1aba42e79f48d3f6358fc0f8425ec8da15ee819d
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2022-09-07 17:22:56 +0200

    Add game logic

--- app/src/main/java/com/example/joyee/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/joyee/MemoryBoardAdapter.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.joyee.models.BoardSize
 import com.example.joyee.models.MemoryCard
@@ -17,13 +19,18 @@ import kotlin.math.min
 class MemoryBoardAdapter(
     private val context: Context,
     private val boardSize: BoardSize,
-    private val cards: List<MemoryCard>
+    private val cards: List<MemoryCard>,
+    private val cardClickListener: CardClickListener
 ) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
     companion object {
         private const val MARGIN_SIZE = 10
         private const val TAG = "MemoryBoardAdapter"
+    }
+
+    interface CardClickListener {
+        fun onCardClicked(position: Int)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -50,8 +57,15 @@ class MemoryBoardAdapter(
         fun bind(position: Int) {
             val memoryCard = cards[position]
             imageButton.setImageResource(if (memoryCard.isFaceUp) memoryCard.identifier else R.drawable.ic_launcher_background)
+
+            imageButton.alpha = if (memoryCard.isMatched) .4f else 1.0f
+            val colorStateList = if (memoryCard.isMatched) ContextCompat.getColorStateList(context, R.color.color_gray) else null
+            ViewCompat.setBackgroundTintList(imageButton, colorStateList)
+
+
             imageButton.setOnClickListener {
                 Log.i(TAG,"Clicked on position $position")
+                cardClickListener.onCardClicked(position)
             }
         }
     }
