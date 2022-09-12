a1ab216ab4ce389116d7e932458022ccc04e61d9
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2022-09-12 17:36:42 +0200

    add permissions to read data storage

--- app/src/main/java/com/example/joyee/ImagePickerAdapter.kt
+++ app/src/main/java/com/example/joyee/ImagePickerAdapter.kt
@@ -12,8 +12,13 @@ import kotlin.math.min
 
 class ImagePickerAdapter(private val context: Context,
                          private val imageUris: List<Uri>,
-                         private val boardSize: BoardSize
+                         private val boardSize: BoardSize,
+                         private val imageClickListener: ImageClickListener
                          ) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
+
+    interface ImageClickListener {
+        fun onPlaceholderClicked()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
@@ -46,6 +51,7 @@ class ImagePickerAdapter(private val context: Context,
         fun bind() {
             ivCustomImage.setOnClickListener {
                 // Launch intent for user to select photos
+                imageClickListener.onPlaceholderClicked()
             }
         }
 
